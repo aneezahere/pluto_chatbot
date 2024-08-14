e806e8a694c0a54b94918eef69c32164a0c10997
Aneeza Shakeel <156274290+aneezahere@users.noreply.github.com>
2024-08-13 20:04:26 -0400

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 // pages/_app.js
+import Head from 'next/head';
 import '../css/signin.css';  // Adjust the path if necessary
 import '../css/dashboard.css';  // Adjust the path if necessary
 import '../css/style.css';  // Adjust the path if necessary
-import '../app/globals.css';  // Adjust the path if necessar
+import '../app/globals.css';  // Adjust the path if necessary
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Pluto.ai - An AI built by Aneeza Shakeel and Ali Hamza" />
+        <meta property="og:title" content="Pluto.ai" />
+        <meta property="og:description" content="An AI built by Aneeza Shakeel and Ali Hamza" />
+        <meta property="og:image" content="https://pluto-chatbotai.vercel.app//images/pluto.webp" />
+        <meta property="og:image:alt" content="Preview of Pluto.ai" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <title>Pluto.ai</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
